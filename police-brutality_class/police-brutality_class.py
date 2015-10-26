0fd471c0412beee5396091313e46eacea0ae3f76
Braxton Gunter <braxton.gun@gmail.com>
2015-10-26 04:45:15 -0400

    Update police-brutality_class.py

--- police-brutality_class/police-brutality_class.py
+++ police-brutality_class/police-brutality_class.py
@@ -129,3 +129,170 @@ def lat_long_coords(address):
 
     return coordinates
 
+# *************************************************************************** #
+
+# AREA INFORMATION
+
+BALTIMORE_ADDRESS = "Baltimore, MD 21030" # Freddie Gray
+
+baltimore_state = state_finder(BALTIMORE_ADDRESS)
+baltimore_county = county_finder(BALTIMORE_ADDRESS)
+baltimore_coords = lat_long_coords(BALTIMORE_ADDRESS)
+maryland_code = state_numeric_codes[baltimore_state]
+
+print(BALTIMORE_ADDRESS)
+pp.pprint(baltimore_state + " (" + str(maryland_code) + ")" + ", " + baltimore_county)
+print("coordinates:", baltimore_coords, "\n")
+
+FERGUSON_ADDRESS = "Ferguson, MO 63135" # Mike Brown
+
+ferguson_state = state_finder(FERGUSON_ADDRESS)
+ferguson_county = county_finder(FERGUSON_ADDRESS)
+ferguson_coords = lat_long_coords(FERGUSON_ADDRESS)
+missouri_code = state_numeric_codes[ferguson_state]
+
+print(FERGUSON_ADDRESS)
+pp.pprint(ferguson_state + " (" + str(missouri_code) + ")" + ", " + ferguson_county)
+print("coordinates:", ferguson_coords, "\n")
+
+CLEVELAND_ADDRESS = "Cleveland, OH 44101" # Tamir Rice
+
+cleveland_state = state_finder(CLEVELAND_ADDRESS)
+cleveland_county = county_finder(CLEVELAND_ADDRESS)
+cleveland_coords = lat_long_coords(CLEVELAND_ADDRESS)
+ohio_code = state_numeric_codes[cleveland_state]
+
+print(CLEVELAND_ADDRESS)
+pp.pprint(cleveland_state + " (" + str(ohio_code) + ")" + ", " + cleveland_county)
+print("coordinates:", cleveland_coords, "\n")
+
+# *************************************************************************** #
+
+# HOUSEHOLD INCOMES & POVERTY PERCENTAGES
+
+# Baltimore
+df_maryland = df_work[(df.State == maryland_code)]
+idx = df_maryland.query("County_Name == 'Baltimore County'").index.tolist()
+
+income_str = df_maryland["Median_Household_Income_in_Dollars"][idx]
+income_str = "".join(income_str)
+
+# convert to int
+mny_chars = ["$",","]
+
+splt = list(income_str)
+for char in splt:
+    if char in mny_chars:
+        splt.remove(char)
+income = int("".join(splt))
+
+baltimore_income = income
+percent_frame = df_maryland["Poverty_Percentage"][idx]
+baltimore_percent = float(percent_frame)
+
+print("%s's County Median Household Income: $" % ("BALTIMORE") + str(baltimore_income))
+print("%s's County Population Percentage in Poverty: %%" % ("BALTIMORE") + str(baltimore_percent) + "\n")
+
+# *************************************************************************** #
+
+# Ferguson
+df_missouri = df_work[(df.State == missouri_code)]
+idx = df_missouri.query("County_Name == 'St. Louis County'").index.tolist()
+
+income_str = df_missouri["Median_Household_Income_in_Dollars"][idx]
+income_str = "".join(income_str)
+
+# convert to int
+mny_chars = ["$",","]
+
+splt = list(income_str)
+for char in splt:
+    if char in mny_chars:
+        splt.remove(char)
+income = int("".join(splt))
+
+ferguson_income = income
+percent_frame = df_missouri["Poverty_Percentage"][idx]
+ferguson_percent = float(percent_frame)
+
+print("%s's County Median Household Income: $" % ("FERGUSON") + str(ferguson_income))
+print("%s's County Population Percentage in Poverty: %%" % ("FERGUSON") + str(ferguson_percent) + "\n")
+
+# *************************************************************************** #
+
+# Cleveland
+df_ohio = df_work[(df.State == ohio_code)]
+idx = df_ohio.query("County_Name == 'Cuyahoga County'").index.tolist()
+
+income_str = df_ohio["Median_Household_Income_in_Dollars"][idx]
+income_str = "".join(income_str)
+
+# convert to int
+mny_chars = ["$",","]
+
+splt = list(income_str)
+for char in splt:
+    if char in mny_chars:
+        splt.remove(char)
+income = int("".join(splt))
+
+cleveland_income = income
+percent_frame = df_ohio["Poverty_Percentage"][idx]
+cleveland_percent = float(percent_frame)
+
+print("%s's County Median Household Income: $" % ("CLEVELAND") + str(cleveland_income))
+print("%s's County Population Percentage in Poverty: %%" % ("CLEVELAND") + str(cleveland_percent) + "\n")
+
+# *************************************************************************** #
+
+# table of information
+
+cities = ["Baltimore", "Ferguson", "Cleveland"]
+counties = [baltimore_county, ferguson_county, cleveland_county]
+incomes = [baltimore_income, ferguson_income, cleveland_income]
+percentages = [baltimore_percent, ferguson_percent, cleveland_percent]
+
+i = {"City": pd.Series(cities), "County": pd.Series(counties), "Household Median Income": pd.Series(incomes), "Percentage of Population in Poverty": pd.Series(percentages)}
+df_main = pd.DataFrame(i)
+
+# ************************************************************************** #
+
+# graph
+
+ax = df_main[["County","Household Median Income"]].plot(kind='bar', title ="Household Median Income",figsize=(15,10),use_index=False)
+ax.set_xlabel("County")
+ax.set_ylabel("Income (in dollars)")
+ax.plot()
+
+ax2 = df_main[["County","Percentage of Population in Poverty"]].plot(kind='bar', title ="Poverty Percentages",figsize=(15,10),use_index=False)
+ax2.set_xlabel("County")
+ax2.set_ylabel("Population in Poverty (%)")
+ax2.plot()
+
+# ************************************************************************** #
+
+# map display
+
+vincent.core.initialize_notebook()
+
+world_topo = r'https://github.com/trifacta/vega/blob/gh-pages/data/world-countries.json'
+state_topo = r'https://github.com/trifacta/vega/blob/gh-pages/data/us-states.json'
+county_geo = r'https://github.com/trifacta/vega/blob/gh-pages/data/us-counties.json'
+county_topo = r'https://github.com/trifacta/vega/blob/gh-pages/data/us-counties.json'
+
+geo_data = [{'name': 'counties',
+             'url': county_topo,
+             'feature': 'us_counties.geo'},
+            {'name': 'states',
+             'url': state_topo,
+             'feature': 'us_states.geo'}
+             ]
+
+vis = vincent.Map(geo_data=geo_data, scale=1000, projection='albersUsa')
+del vis.marks[1].properties.update
+vis.marks[0].properties.update.fill.value = '#084081'
+vis.marks[1].properties.enter.stroke.value = '#fff'
+vis.marks[0].properties.enter.stroke.value = '#7bccc4'
+vis.to_json('vega.json')
+
+vis.display()
