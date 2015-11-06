2c86eb16ec8681927ea355feb675e7cf5e902503
Braxton Gunter <braxton.gun@gmail.com>
2015-11-06 17:01:57 -0500

    colors

---
+++ slither/colors.py
@@ -0,0 +1,28 @@
+##	RGB Values for color
+##	Braxton Gunter
+
+"""This module provides the RGB values for string colors to be imported into 
+other code ***THE COLOR CONSTANTS ARE IN ALL CAPS***"""
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+RED = (255,0,0)
+LIME = (0,255,0)
+BLUE = (0,0,255)
+
+YELLOW = (255,255,0)
+AQUA = CYAN = (0,255,255)
+MAGENTA = FUSCHIA =  (255,0,255)
+
+SILVER = (192,192,192)
+GREY = GRAY = (128,128,128)
+
+MAROON = (128,0,0)
+OLIVE = (128,128,0)
+GREEN = (0,128,0)
+
+PURPLE = (128,0,128)
+TEAL = (0,128,128)
+NAVY = (0,0,128)
+
